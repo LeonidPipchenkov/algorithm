4a40094e7baedfb755bafeb05e0d6d8096117be2
Leonid Pipchenkov <t70a80@gmail.com>
2024-06-23 16:16:29 +0300

    LeetCode 1552

---
+++ src/main/java/magnetic_force_between_1552/Main.java
@@ -0,0 +1,12 @@
+package magnetic_force_between_1552;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        int[] position = {1, 2, 3, 4, 7};
+        int result = solution.maxDistance(position, 3);
+        System.out.println(result);
+    }
+
+}
